00928095beed577fef428c74a028de5af14bffae
Luke G <lukegronert@gmail.com>
2020-12-24 15:22:53 +0800

    Completes sumAll exercise

--- javascript-exercises-master/sumAll/sumAll.js
+++ javascript-exercises-master/sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
-
+const sumAll = function(x,y) {
+    if(typeof(x) === "number" && typeof(y) === "number" && x>0 && y> 0) {
+        answer = 0;
+        if(x < y) {
+            for (let i = x; i<=y; i++) {
+                answer += i;
+            }
+        } else {
+            for (let j = y; j<=x; j++) {
+                answer += j;
+            }
+        }
+        return answer;
+    } else {
+        return "ERROR";
+    }
 }
 
 module.exports = sumAll
