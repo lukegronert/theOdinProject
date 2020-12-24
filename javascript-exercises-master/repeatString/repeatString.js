2d04849f1f4d9426160c5447b7a0a36f11831b6f
Luke G <lukegronert@gmail.com>
2020-12-24 14:45:12 +0800

    Finished helloWorld, repeatString, and reverseString exercises

--- javascript-exercises-master/repeatString/repeatString.js
+++ javascript-exercises-master/repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+    let answer = '';
+    if (num<0) {
+        answer = 'ERROR';
+    } else {
+        while(num > 0) {
+            answer+=str;
+            num--;
+        }
+    }
+    return answer;
 }
 
 module.exports = repeatString
--- javascript-exercises-master/reverseString/reverseString.js
+++ javascript-exercises-master/reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let answer = '';
+    for(let i = (str.length-1); i >=0; i--) {
+        answer += str.slice(i, i+1)
+    }
+    return answer;
 }
 
 module.exports = reverseString
