ea8d0a4e6274876b927a35dfe1427fcff0bca96e
Luke G <lukegronert@gmail.com>
2020-12-24 15:39:26 +0800

    Completed tempConversion exercise

--- javascript-exercises-master/tempConversion/tempConversion.js
+++ javascript-exercises-master/tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(temp) {
+  let answer = Math.round(((temp -32)*(5/9)*10));
+  return answer / 10;
 }
 
-const ctof = function() {
-
+const ctof = function(temp) {
+  let answer = Math.round(((temp*(9/5)) + 32)*10)
+  return answer / 10;
 }
 
 module.exports = {
