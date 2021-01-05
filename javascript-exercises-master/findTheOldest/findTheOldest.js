89762a9ab7f5390429afa61e9ed8a3d6616afe9c
Luke G <lukegronert@gmail.com>
2021-01-05 16:47:30 +0800

    Finished findTheOldest exercise, code needs to be reworked as it is way too long

--- javascript-exercises-master/findTheOldest/findTheOldest.js
+++ javascript-exercises-master/findTheOldest/findTheOldest.js
@@ -1,5 +1,32 @@
-let findTheOldest = function() {
-
+let findTheOldest = function(arr) {
+    let answer = arr.sort((a,b) => {
+        if(!a.hasOwnProperty('yearOfDeath')) {
+            let aAge = (new Date()).getFullYear() - a.yearOfBirth;
+            let bAge = b.yearOfDeath - b.yearOfBirth;
+            if(aAge > bAge) {
+                return -1
+            } else {
+                return 1
+            }
+        } else if (!b.hasOwnProperty('yearOfDeath')) {
+            let aAge = a.yearOfDeath - a.yearOfBirth;
+            let bAge = (new Date()).getFullYear() - b.yearOfBirth;
+            if(aAge > bAge) {
+                return -1
+            } else {
+                return 1
+            }
+        } else {
+            let aAge = a.yearOfDeath - a.yearOfBirth;
+            let bAge = b.yearOfDeath - b.yearOfBirth;
+            if(aAge > bAge) {
+                return -1
+            } else {
+                return 1
+            }
+        }
+    });
+    return answer[0]
 }
 
 module.exports = findTheOldest
