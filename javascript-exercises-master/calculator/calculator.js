2470ad80b80bfbd8982277fe311f26dacbbdc8c9
Luke G <lukegronert@gmail.com>
2021-01-05 13:00:06 +0800

    Completed calculator javascript exercise

--- javascript-exercises-master/calculator/calculator.js
+++ javascript-exercises-master/calculator/calculator.js
@@ -1,25 +1,41 @@
-function add () {
-	
+function add (a,b) {
+	return a + b;
 }
 
-function subtract () {
-	
+function subtract (a,b) {
+	return a-b;
 }
 
-function sum () {
-	
+function sum (arr) {
+	let sumTotal = 0;
+	for (let i =0; i<arr.length; i++) {
+		sumTotal += arr[i];
+	}
+	return sumTotal;
 }
 
-function multiply () {
-	
+function multiply (arr) {
+	let multiplyTotal = arr[0];
+	for (let i =1; i<arr.length; i++) {
+		multiplyTotal *= arr[i];
+	}
+	return multiplyTotal;
 }
 
-function power() {
-	
+function power(a,b) {
+	return a**b;
 }
 
-function factorial() {
-	
+function factorial(num) {
+	if(num === 0 || num === 1) {
+		return 1;
+	} else {
+		let factorialTotal = num;
+		for(let i=num-1; i>0;i--) {
+			factorialTotal *= i;
+		}
+		return factorialTotal;
+	}
 }
 
 module.exports = {
