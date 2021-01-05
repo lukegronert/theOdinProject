9a98fbd2430aaff5b36b8211304095b271e4dce2
Luke G <lukegronert@gmail.com>
2021-01-05 15:33:36 +0800

    Finished fibonacci exercise

--- javascript-exercises-master/fibonacci/fibonacci.js
+++ javascript-exercises-master/fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    let arr = [1];
+    if(n < 1) {
+        return "OOPS";
+    } else {
+        for(let i=0; i<n;i++) {
+            if(arr.length === 1) {
+                arr.push(1);
+            } else {
+                arr.push(arr[i]+arr[i-1])
+            }
+        }
+        return arr[n-1];
+    }
 }
 
 module.exports = fibonacci
