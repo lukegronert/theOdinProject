2735e46b51fe884fe43857961d5d958f98520555
Luke G <lukegronert@gmail.com>
2021-01-05 14:10:31 +0800

    Finished palindromes exercise (study .replace(/[^A-Za-z]/g, '');

--- javascript-exercises-master/palindromes/palindromes.js
+++ javascript-exercises-master/palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function() {
-
+const palindromes = function(word) {
+    let newWord = word.toLowerCase().replace(/[^A-Za-z]/g, "");
+    let wordArr = newWord.split('');
+    wordArr.reverse();
+    let reversedWord = wordArr.join("");
+    return reversedWord == newWord;
 }
 
 module.exports = palindromes
