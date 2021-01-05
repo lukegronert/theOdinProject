56e61927733535a54f228df86d7ced1dba850c08
Luke G <lukegronert@gmail.com>
2021-01-05 16:07:07 +0800

    Finished getTheTitles exercise

--- javascript-exercises-master/getTheTitles/getTheTitles.js
+++ javascript-exercises-master/getTheTitles/getTheTitles.js
@@ -1,5 +1,7 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(arr) {
+    let titles = [];
+    arr.map((book) => titles.push(book.title))
+    return titles;
 }
 
 module.exports = getTheTitles;
