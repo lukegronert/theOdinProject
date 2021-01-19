b4c6aee0f49c7d65728102da231080579c1d0452
Luke G <lukegronert@gmail.com>
2021-01-19 12:24:41 +0800

    Added DOM, Gameboard, and Controller Modules. Player can now add Xs and Os. No win check has been added.

--- projects/ticTacToeApp/script.js
+++ projects/ticTacToeApp/script.js
@@ -1,19 +1,61 @@
-const Gameboard = (() => {
-    const board = [];
-    const gameboard = document.querySelector("#gameboard");
-    const newSquare = () => {
-        const square = document.createElement('div');
-        square.classList.add('square');
-        return square;
-    }
-    const setupBoard = () => {
-        for(let i = 0; i<9;i++) {
-            gameboard.appendChild(newSquare());
-        }
-    };
+const DOM = (() => {
     return {
-        setupBoard
+        gameboard: document.querySelector("#gameboard"),
+        newSquare: () => {
+            const square = document.createElement('div');
+            square.classList.add('square');
+            return square;
+        },
+        setupBoard: () => {
+            for(let i = 0; i<9;i++) {
+                gameboard.appendChild(DOM.newSquare());
+            }
+        },
+        getSquares: () => {
+            return this.gameboard.querySelectorAll('.square')
+        }
     }
 })();
 
-Gameboard.setupBoard();
+const Gameboard = (() => {
+    const board = ['','','','','','','','',''];
+    const getBoard = () => {
+        return board;
+    }
+    return {
+        getBoard
+    }
+})();
+
+const Controller = (() => {
+    let mark = 'O'
+    const markChange = () => {
+        if(mark === 'X') {
+            mark = 'O'
+            return mark
+        } else {
+            mark = 'X'
+            return mark
+        }
+    }
+    const placeMarker = () => {
+        DOM.getSquares().forEach((square) => {
+            square.addEventListener('click', () => {
+                square.textContent = markChange();
+            })
+        })
+    }
+    const updateBoard = () => {
+        for(let i=0;i<9;i++) {
+            Gameboard.getBoard()[i] = DOM.getSquares()[i].textContent;
+        }
+    }
+    return {
+        placeMarker,
+        updateBoard,
+    }
+})();
+
+
+DOM.setupBoard();
+Controller.placeMarker();
