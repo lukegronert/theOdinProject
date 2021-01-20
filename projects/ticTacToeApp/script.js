9f2ecc0c679001d133e075ebb87c9acd75014d6b
Luke G <lukegronert@gmail.com>
2021-01-20 11:01:29 +0800

    Added checkWinner which checks to see if either player has won and alerts the result if so

--- projects/ticTacToeApp/script.js
+++ projects/ticTacToeApp/script.js
@@ -38,21 +38,45 @@ const Controller = (() => {
             return mark
         }
     }
-    const placeMarker = () => {
-        DOM.getSquares().forEach((square) => {
-            square.addEventListener('click', () => {
-                square.textContent = markChange();
-            })
-        })
-    }
     const updateBoard = () => {
         for(let i=0;i<9;i++) {
             Gameboard.getBoard()[i] = DOM.getSquares()[i].textContent;
         }
     }
+    const placeMarker = () => {
+        DOM.getSquares().forEach((square) => {
+            square.addEventListener('click', () => {
+                if(square.textContent == '') {
+                    square.textContent = markChange();
+                    updateBoard();
+                    checkWinner();
+                }
+            })
+        })
+    }
+    const checkWinner = () => {
+        const winConditions = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6],
+        ]
+        for(let i=0; i<8;i++) {
+            if(Gameboard.getBoard()[winConditions[i][0]] == 'X' && Gameboard.getBoard()[winConditions[i][1]] == 'X' && Gameboard.getBoard()[winConditions[i][2]] == 'X') {
+                return alert('X WINS');
+            } else if(Gameboard.getBoard()[winConditions[i][0]] == 'O' && Gameboard.getBoard()[winConditions[i][1]] == 'O' && Gameboard.getBoard()[winConditions[i][2]] == 'O') {
+                return alert('O WINS');
+            }
+        }
+    }
     return {
         placeMarker,
         updateBoard,
+        checkWinner
     }
 })();
 
