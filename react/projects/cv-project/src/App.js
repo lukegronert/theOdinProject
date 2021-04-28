dbea3d091e90572528eee53b855338ac9d60ef7f
Luke G <lukegronert@gmail.com>
2021-04-28 13:27:05 +0800

    Styled the display of the cv information with gridCSS in tailwind. Removed labels from info entry side of the app and replaced them with placeholders in the inputs. Made submit buttons 100% of the width of thei respective columns.

--- react/projects/cv-project/src/App.js
+++ react/projects/cv-project/src/App.js
@@ -172,98 +172,93 @@ export default class App extends Component {
       <h1 className='text-center'>CV</h1>
         <div className='grid grid-cols-5 shadow-lg border-2 rounded border-gray-400 border-solid p-2 w-11/12 divide-y container mx-auto'>
           <div className='col-start-1 col-end-3 border-r-2 border-black border-solid'>
-          <h2>General</h2>
+          <h2 className='text-xl font-semibold'>General</h2>
           <form onSubmit={this.onSubmitGeneral}>
               <div>
                 <div>
-                  <label>Name: </label>
-                  <input type="text" name="fullName"
+                  <input className='w-full border rounded' placeholder='Name' type="text" name="fullName"
                     value={this.state.generalForm.fullName}
                     onChange={this.handleGeneralChange}></input>
                 </div>
                 <div>
-                  <label>Email: </label>
-                  <input type="text" name='email'
+                  <input className='w-full border rounded' placeholder='Email' type="text" name='email'
                     value={this.state.generalForm.email}
                     onChange={this.handleGeneralChange}></input>
                 </div>
                 <div>
-                  <label>Phone Number: </label>
-                  <input type="text" name='phone'
+                  <input className='w-full border rounded' placeholder='Phone Number' type="text" name='phone'
                     value={this.state.generalForm.phone}
                     onChange={this.handleGeneralChange}></input>
                 </div>
                 <div className='content-center'>
-                  <button className='bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow' 
+                  <button className='w-full bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow' 
                   type='submit'>Submit</button>
                 </div>
               </div>
           </form>
 
-          <h2>Education</h2>
+          <h2 className='text-xl font-semibold'>Education</h2>
           <form onSubmit={this.onSubmitEducation}>
           <div>
-            <label>School Name: </label>
-            <input type="text" name="school"
+            <input className='w-full border rounded' placeholder='School Name' type="text" name="school"
               value={this.state.education.school}
               onChange={this.handleEducationChange}></input>
           </div>
           <div>
-            <label>Degree/Certificate: </label>
-            <input type="text" name="degree"
+            <input className='w-full border rounded' placeholder='Degree/Certificate' type="text" name="degree"
               value={this.state.education.degree}
               onChange={this.handleEducationChange}></input>
           </div>
           <div>
-            <label>Dates Attended: </label>
-            <input type="text" name="date"
+            <input className='w-full border rounded' placeholder='Dates Attended' type="text" name="date"
               value={this.state.education.date}
               onChange={this.handleEducationChange}></input>
           </div>
-            <button className='bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow'
+            <button className='w-full bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow'
             type='submit'>Add Education</button>
           </form>
 
-          <h2>Experience</h2>
+          <h2 className='text-xl font-semibold'>Experience</h2>
           <form onSubmit={this.onSubmitExperience}>
           <div>
-            <label>Company: </label>
-            <input type="text" name="company"
+            <input className='w-full border rounded' placeholder='Company' type="text" name="company"
               value={this.state.experience.company}
               onChange={this.handleExperienceChange}></input>
           </div>
           <div>
-            <label>Position: </label>
-            <input type="text" name="position"
+            <input className='w-full border rounded' placeholder='Position' type="text" name="position"
               value={this.state.experience.position}
               onChange={this.handleExperienceChange}></input>
           </div>
           <div>
-            <label>Description: </label>
-            <input type="text" name="description"
+            <textarea className='w-full border rounded' placeholder='Description' type="text" name="description"
               value={this.state.experience.description}
-              onChange={this.handleExperienceChange}></input>
+              onChange={this.handleExperienceChange}></textarea>
           </div>
           <div>
-            <label>Start Date: </label>
-            <input type="text" name="startDate"
+            <input className='w-full border rounded' placeholder='Start Date' type="text" name="startDate"
               value={this.state.experience.startDate}
               onChange={this.handleExperienceChange}></input>
           </div>
           <div>
-            <label>End Date: </label>
-            <input type="text" name="endDate"
+            <input className='w-full border rounded' placeholder='End Date' type="text" name="endDate"
               value={this.state.experience.endDate}
               onChange={this.handleExperienceChange}></input>
           </div>
-            <button className='bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow'
+            <button className='w-full bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow'
             type='submit'>Add Experience</button>
           </form>
         </div>
-        <div className="col-start-3 col-end-6 border-none pl-2">
-          <General info={generalInfo} onEdit={this.onEditGeneral} />
+        <div className="grid grid-cols-4 grid-rows-5  col-start-3 col-end-6 border-none pl-2">
+          <div className='row-start-1 row-end-2'>
+          <General  info={generalInfo} onEdit={this.onEditGeneral} />
+          </div>
+          <div className='col-start-1 col-end-3 row-start-2 row-end-6 p-1 break-words'>
           <Education info={educationList} onEdit={this.onEditEducation} onDelete={this.onDeleteEducation} />
-          <Experience info={experienceList} onEdit={this.onEditExperience} onDelete={this.onDeleteExperience} />
+          </div>
+          <div className='col-start-3 col-end-5 row-start-2 row-end-6 p-1 break-words'>
+          <Experience className='col-start-4 col-end-6' info={experienceList} onEdit={this.onEditExperience} onDelete={this.onDeleteExperience} />
+          </div>
         </div>
           </div>
       </div>
