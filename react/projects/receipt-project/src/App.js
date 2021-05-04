14997473108042b248220035554c44adccfaa71c
Luke G <lukegronert@gmail.com>
2021-05-04 13:55:12 +0800

    Added handleChange and onSubmit methods to App.js. Order information is sent to Receipt component and rendered to Receipt div. The total price is added up as orders are submitted.

--- react/projects/receipt-project/src/App.js
+++ react/projects/receipt-project/src/App.js
@@ -1,44 +1,87 @@
 import React, { Component } from 'react';
 import { FaCocktail, FaCoffee, FaHamburger, FaHotdog, FaPizzaSlice, FaIceCream, FaCookie } from "react-icons/fa";
 import Receipt from './components/Receipt.js';
+import uniqid from 'uniqid';
 
 export default class App extends Component {
   constructor(props) {
     super();
 
     this.state = {
-      id: '',
-      name: '',
-      burger: '',
-      dessert: '',
-      drink: '',
+      orderInput: {
+        id: uniqid(),
+        name: '',
+        main: '',
+        mainPrice: [],
+        dessert: '',
+        dessertPrice: [],
+        drink: '',
+        drinkPrice: [],
+      },
+      orderTotal: [],
     }
-
   }
+
+  handleChange = (e) => {
+    const category = e.target.name;
+    const categoryPrice = category + 'Price';
+    this.setState({
+      orderInput: {
+        ...this.state.orderInput,
+        [category]: e.target.value,
+        [categoryPrice]: e.target.dataset.price,
+      }
+    })
+    console.log(this.state)
+  }
+
+  onSubmit = (e) => {
+    e.preventDefault();
+    this.setState({
+      orderTotal: this.state.orderTotal.concat(this.state.orderInput),
+      orderInput: {
+        id: uniqid(),
+        name: '',
+        main: '',
+        mainPrice: [],
+        dessert: '',
+        dessertPrice: [],
+        drink: '',
+        drinkPrice: [],
+      }
+    })
+    setTimeout(() => {
+      const form = document.querySelector('form');
+      form.reset();
+    }, 100)
+    console.log(this.state.orderTotal)
+  }
+
   render() {
+    const { orderTotal } = this.state;
     return (
       <div className='appContainer'>
-        <form className='formContainer'>
+        <form className='formContainer' onSubmit={this.onSubmit}>
           <h1>Place your order:</h1>
           <div className='choiceRowDiv'>
             <div className='choiceDiv'>
               <label><FaHamburger className='icon' /><br />
               <span className='price'>$4.99</span><br />
-              <input type="radio" name='burger' value='hamburger'></input>
+              <input onChange={this.handleChange} type="radio" name='main' value='hamburger' data-price={4.99}></input>
               </label>
             </div>
             <div className='choiceDiv'>
               <label><FaPizzaSlice className='icon' />
               <br />
               <span className='price'>$3.99</span><br />
-              <input type="radio" name='burger' value='pizza'></input>
+              <input onChange={this.handleChange} type="radio" name='main' value='pizza' data-price={3.99}></input>
               </label>
             </div>
             <div className='choiceDiv'>
               <label><FaHotdog className='icon' />
               <br />
               <span className='price'>$2.50</span><br />
-              <input type="radio" name='burger' value='hotdog'></input>
+              <input onChange={this.handleChange} type="radio" name='main' value='hotdog' data-price={2.50}></input>
               </label>
             </div>
           </div>
@@ -47,14 +90,14 @@ export default class App extends Component {
               <label><FaIceCream className='icon' />
               <br />
                 <span className='price'>$1.99</span><br />
-              <input type="radio" name='dessert' value='icecream'></input>
+              <input onChange={this.handleChange} type="radio" name='dessert' value='icecream' data-price={1.99}></input>
                 </label>
             </div>
             <div className='choiceDiv'>
               <label><FaCookie className='icon' />
               <br />
                 <span className='price'>$0.50</span><br />
-              <input type="radio" name='dessert' value='cookie'></input>
+              <input onChange={this.handleChange} type="radio" name='dessert' value='cookie' data-price={0.50}></input>
                 </label>
             </div>
           </div>
@@ -63,21 +106,21 @@ export default class App extends Component {
               <label><FaCoffee className='icon' />
               <br />
                 <span className='price'>$0.99</span><br />
-              <input type="radio" name='drink' value='coffee'></input>
+              <input onChange={this.handleChange} type="radio" name='drink' value='coffee' data-price={0.99}></input>
                 </label>
             </div>
             <div className='choiceDiv'>
               <label><FaCocktail className='icon' />
               <br />
                 <span className='price'>$5.99</span><br />
-              <input type="radio" name='drink' value='cocktail'></input>
+              <input onChange={this.handleChange} type="radio" name='drink' value='cocktail' data-price={5.99}></input>
                 </label>
             </div>
           </div>
           <input type='submit'></input>
         </form>
 
-        <Receipt />
+        <Receipt order={orderTotal} />
 
       </div>
   )}
--- react/projects/receipt-project/src/components/Receipt.js
+++ react/projects/receipt-project/src/components/Receipt.js
@@ -1,9 +1,25 @@
 import React from 'react'
 
-export default function Receipt() {
+export default function Receipt(props) {
+    const totalPrice = () => {
+        let sum = 0;
+        props.order.map((item) => {
+            sum += Number(item.mainPrice) + Number(item.drinkPrice) + Number(item.dessertPrice)
+        })
+        return sum.toFixed(2);
+    }
     return (
         <div className='receiptContainer'>
             <h1>Receipt</h1>
+            {props.order.map((item) => {
+                return (<div key={item.id}>
+                    <p>{item.main} - {item.mainPrice}</p>
+                    <p>{item.dessert} - {item.dessertPrice}</p>
+                    <p>{item.drink} - {item.drinkPrice}</p>
+                    </div>)
+            })}
+            <h4>Total: {totalPrice()}
+            </h4>
         </div>
     )
 }
