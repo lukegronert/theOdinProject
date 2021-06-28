c7735d9ad8deffc1f4248a7247803dda522ed0ad
Luke G <lukegronert@gmail.com>
2021-06-28 14:50:43 +0800

    Created a layout and included sample text for each area, to later be filled with api info. Also created async getWeather function to get the data.

--- projects/WeatherAPI/script.js
+++ projects/WeatherAPI/script.js
@@ -1,1 +1,13 @@
-console.log('Connected')
+const api_key = '30303c4b0a5b602c9cdab00b9975eef4';
+const searchBar = document.querySelector('input');
+const submitButton = document.querySelector('#submitButton');
+
+async function getWeather(city) {
+    const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key}`)
+    return response.json();
+}
+
+submitButton.addEventListener('click', () => {
+    const city = searchBar.value;
+    getWeather(city).then(response => console.log(response));
+})
