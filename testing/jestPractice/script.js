5d0857534537e9351baa525af3bc8f33e27cf0a3
Luke G <lukegronert@gmail.com>
2021-07-04 18:44:36 +0800

    Did all practice testing. Will need to add additional testing and separate function and tests into separate files/folders

--- testing/jestPractice/script.js
+++ testing/jestPractice/script.js
@@ -26,14 +26,64 @@ const calculator = {
             return acc - curr;
         })
     },
-    multiply: () => {
-
+    multiply: (...args) => {
+        return args.reduce((acc, curr) => {
+            return acc * curr;
+        })
     },
-    divide: () => {
-
+    divide: (...args) => {
+        return args.reduce((acc, curr) => {
+            return acc / curr;
+        })
     }
+}
+
+function caesarCypher(str, key) {
+    const abc = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+    const ABC = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+    const strArr = str.split('');
+    const cypherArr = [];
+    for(let i=0; i<strArr.length; i++) {
+        if(abc.includes(strArr[i])) {
+            if(abc.indexOf(strArr[i]) + key > 25) {
+                cypherArr.push(abc[abc.indexOf(strArr[i])+key-25])
+            } else {
+                cypherArr.push(abc[abc.indexOf(strArr[i])+key])
+            }
+        } else if(ABC.includes(strArr[i])) {
+            if(ABC.indexOf(strArr[i]) + key > 25) {
+                cypherArr.push(ABC[ABC.indexOf(strArr[i])+key-25])
+            } else {
+                cypherArr.push(ABC[ABC.indexOf(strArr[i])+key])
+            }
+        } else {
+            cypherArr.push(strArr[i]);
+        }
+    }
+    return cypherArr.join('');
+}
+
+function analyze(...args) {
+    let object = {};
+    object.average = (args.reduce((acc, curr) => {
+        return acc + curr
+    }))/args.length;
+
+    object.minimum = (args.reduce((acc,curr) => {
+        return Math.min(acc,curr);
+    }))
+
+    object.maximum = (args.reduce((acc,curr) => {
+        return Math.max(acc,curr);
+    }))
+
+    object.length = args.length;
+    
+    return object;
 }
 
 exports.capitalize = capitalize;
 exports.reverseString = reverseString;
 exports.calculator = calculator;
+exports.caesarCypher = caesarCypher;
+exports.analyze = analyze;
--- testing/jestPractice/script.test.js
+++ testing/jestPractice/script.test.js
@@ -1,25 +1,45 @@
 const modules = require('./script');
 
-test('Takes string and returns String', () => {
+test('capitalize function takes string and returns String', () => {
     expect(modules.capitalize('string')).toBe('String');
 });
 
-test('Takes string and return gnirts', () => {
+test('reverseString function takes string and return gnirts', () => {
     expect(modules.reverseString('string')).toBe('gnirts');
 })
 
-test('Takes numbers and adds them together', () => {
+test('calculator.add takes numbers and adds them together', () => {
     expect(modules.calculator.add(1,5,8)).toBe(14);
 })
 
-test('Takes numbers and subtracts them together', () => {
+test('calculator.subtract takes numbers and subtracts them together', () => {
     expect(modules.calculator.subtract(10,2,3)).toBe(5);
 })
 
-test('Takes numbers and multiplies them together', () => {
-    
+test('calculator.multiply takes numbers and multiplies them together', () => {
+    expect(modules.calculator.multiply(2,2,10)).toBe(40);
 })
 
-test('Takes numbers and divides them', () => {
-    
+test('calculator.divide takes numbers and divides them', () => {
+    expect(modules.calculator.divide(2,2,10)).toBe(0.1);
+})
+
+test('caesarCypher returns cypher depending on key number', () => {
+    expect(modules.caesarCypher('Hey, Jake', 2)).toBe('Jgb, Lcmg');
+})
+
+test('analyze().average returns average', () => {
+    expect(modules.analyze(2,4,6,8).average).toBe(5);
+})
+
+test('analyze().minimum returns minimum', () => {
+    expect(modules.analyze(2,4,6,8).minimum).toBe(2);
+})
+
+test('analyze().maximum returns maximum', () => {
+    expect(modules.analyze(2,4,6,8).maximum).toBe(8);
+})
+
+test('analyze().length returns length', () => {
+    expect(modules.analyze(2,4,6,8).length).toBe(4);
 })
