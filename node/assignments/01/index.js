2bb2d4b30fd85c155613ecf5212c8a60288c4453
Luke G <lukegronert@gmail.com>
2022-03-03 18:24:31 +0800

    Practiced writing the solution from memory. Tried different solutions.

--- node/assignments/01/index.js
+++ node/assignments/01/index.js
@@ -1,47 +1,28 @@
 const http = require('http');
 const fs = require('fs');
-const url = require('url');
 
-const port = 8080;
+const port = 3000;
 
-const server = http.createServer((req, res) => {
-    let requestedUrl = url.parse(req.url);
-    if(requestedUrl.pathname === '/') {
-        fs.readFile('./index.html', (err, data) => {
-            if (err) {
-                res.writeHead(404, {'Content-Type': 'text/html'});
-                return res.end('<h1>Error 404</h1><p>Page not found.</h1>')
-            }
-            res.writeHead(200, {'Content-Type': 'text/html'});
-            res.write(data)
-            return res.end()
-        })
-    } else if(requestedUrl.pathname === '/about.html') {
-        fs.readFile('./about.html', (err, data) => {
-            if(err) {
-                res.writeHead(404, {'Content-Type': 'text/html'});
-                return res.end('<h1>Error 404</h1><p>Page not found.</h1>')
-            }
-            res.writeHead(200, {'Content-Type': "text/html"})
-            res.write(data)
-            return res.end()
-        })
-    } else if(requestedUrl.pathname === '/contact-me.html') {
-        fs.readFile('./contact-me.html', (err, data) => {
-            if (err) {
-                res.writeHead(404, {'Content-Type': 'text/html'});
-                return res.end('<h1>Error 404</h1><p>Page not found.</h1>')
-            }
-            res.writeHead(200, {'Content-Type': 'text/html'})
-            res.write(data)
-            return res.end()
-        })
-    } else {
-        res.writeHead(404, {'Content-Type': 'text/html'});
-        return res.end('<h1>Error 404</h1><p>Page not found.</h1><a href="/">Back home</a>')
+const page404 = fs.readFileSync('./404.html', (err, data) => {
+    if(err) {
+        console.log(err)
     }
+    return data;
 })
 
-server.listen(port, () => {
-    console.log(`Server running at port ${port}`);
+const server = http.createServer((req, res) => {
+    const filename = req.url === '/' ? __dirname + '/index.html' : __dirname + `${req.url}.html`;
+    console.log(req.url)
+    fs.readFile(filename, (err, data) => {
+        if(err) {
+            res.writeHead(404)
+            return res.end(page404)
+        }
+        res.writeHead(200)
+        return res.end(data)
+    })
+})
+
+server.listen(3000, () => {
+    console.log(`Server running on localhost:${port}.`)
 })
