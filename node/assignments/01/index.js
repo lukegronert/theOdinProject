863093deafee3d642cdbb1a26e397a4b00365302
Luke G <lukegronert@gmail.com>
2022-03-22 12:09:07 +0800

    Rewrote basic server routing with express.

--- node/assignments/01/index.js
+++ node/assignments/01/index.js
@@ -1,28 +1,46 @@
-const http = require('http');
 const fs = require('fs');
-
+const express = require('express');
+const app = express();
 const port = 3000;
 
 const page404 = fs.readFileSync('./404.html', (err, data) => {
     if(err) {
         console.log(err)
     }
-    return data;
+    return data
 })
 
-const server = http.createServer((req, res) => {
-    const filename = req.url === '/' ? __dirname + '/index.html' : __dirname + `${req.url}.html`;
-    console.log(req.url)
-    fs.readFile(filename, (err, data) => {
+app.get('/about', (req, res) => {
+    fs.readFile('./about.html', (err, data) => {
         if(err) {
-            res.writeHead(404)
-            return res.end(page404)
+            console.log(err)
         }
-        res.writeHead(200)
         return res.end(data)
     })
 })
 
-server.listen(3000, () => {
-    console.log(`Server running on localhost:${port}.`)
+app.get('/contact', (req, res) => {
+    fs.readFile('./contact-me.html', (err, data) => {
+        if(err) {
+            console.log(err)
+        }
+        return res.end(data)
+    })
+})
+
+app.get('/', (req, res) => {
+    fs.readFile('./index.html', (err, data) => {
+        if(err) {
+            console.log(err)
+        }
+        return res.end(data)
+    })
+})
+
+app.use((req, res, next) => {
+    res.status(404).end(page404)
+})
+
+app.listen(port, () => {
+    console.log(`Listening on port: ${port}.`)
 })
